871c2ff86b753fc859fec11ce48d56a5914fd604
szc-0815 <shizhichao5@outlook.com>
2020-05-18 20:14:43 +0800

    上传zuul网关跨域访问

---
+++ zuul/src/main/java/com/one/shop/zuul/config/CorsConfig.java
@@ -0,0 +1,35 @@
+package com.one.shop.zuul.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+/**
+ * 跨域配置类
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean(){
+        //跨域配置
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("*"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+        corsConfiguration.setMaxAge(3600L);
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        //配置过滤器
+        CorsFilter corsFilter = new CorsFilter(source);
+        //注册过滤器
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(corsFilter);
+        filterRegistrationBean.setOrder(0);
+        return filterRegistrationBean;
+    }
+}
