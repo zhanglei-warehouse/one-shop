0eb7736df0ba209ae1fae337f336d87c3db9d895
szc-0815 <shizhichao5@outlook.com>
2020-05-19 21:27:57 +0800

    shop-user项目的框架第二次上传

---
+++ shop-user/src/main/java/com/one/shop/config/MyWebMvcConfigurer.java
@@ -0,0 +1,25 @@
+package com.one.shop.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
+
+
+
+
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/img/**").addResourceLocations("file:D:\\pic\\");
+
+    }
+
+
+
+}
